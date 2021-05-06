e7c147f77b7e0fd7408e6650fb508fad75b435df
YZ <zhyzh91@gmail.com>
2021-05-05 22:12:51 -0400

    Update preprocess.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -10,29 +10,27 @@ import cv2
 import openslide
 import time
 
-path_ori = '.../Originals_and_No_annt/'
-path_ant = '.../Annotations Only/'
 PATCH_SIZE = 224
 STRIDE = 224
 DOWN_SIZE = 508
 
-def preprocess_mask(path_ori, width_resize=2560, height_resize=1920):
+def preprocess_mask(path_ori, path_mask, width_resize=2560, height_resize=1920):
     start_time = time.time()
     all_files = os.listdir(path_ori)
     for file_ in all_files:
         ori = openslide.OpenSlide(os.path.join(path_ori, file_))
         name = file_.split('.')[0]
-        if os.path.exists(os.path.join(path_ant, '{}-annotations.png'.format(name))):
-            annt = Image.open(os.path.join(path_ant, '{}-annotations.png'.format(name)))
+        if os.path.exists(os.path.join(path_mask, '{}-annotations.png'.format(name))):
+            annt = Image.open(os.path.join(path_mask, '{}-annotations.png'.format(name)))
             width, height = annt.size
             width_r = width/width_resize
             height_r = height/height_resize
             annt_resize = annt.resize((width_resize,height_resize))
             annt_resize_img = np.array(annt_resize)
-            if not os.path.exists('data/annotations/'):
-                os.makedirs('data/annotations/')
-            cv2.imwrite('data/annotations/{}_annt.jpg'.format(name), annt_resize_img)
+            os.makedirs(os.path.join('data/masks/'), exist_ok=True)
+            cv2.imwrite('data/masks/{}_annt.jpg'.format(name), annt_resize_img)
         print('{} finished'.format(name))
+        time.s
     print("--- %s seconds ---" % (time.time() - start_time_total))
 
 def preprocess_global(path_ori, width_resize=2560, height_resize=1920):
@@ -46,8 +44,8 @@ def preprocess_global(path_ori, width_resize=2560, height_resize=1920):
         x_resize_maxs = []
         y_resize_mins = []
         y_resize_maxs = []
-        if os.path.exists('data/annotations/{}_annt.jpg'.format(name)):
-            annt_resized = cv2.imread('data/annotations/{}_annt.jpg'.format(name), 0)
+        if os.path.exists('data/masks/{}_annt.jpg'.format(name)):
+            annt_resized = cv2.imread('data/masks/{}_annt.jpg'.format(name), 0)
             height_annt, width_annt = annt_resized.shape
             ret, score = cv2.threshold(annt_resized, 128, 1, 0)
             nLabels, labels, stats, centroids = cv2.connectedComponentsWithStats(score.astype(np.uint8), connectivity=4)
@@ -92,17 +90,16 @@ def preprocess_global(path_ori, width_resize=2560, height_resize=1920):
             crop = crop.resize((DOWN_SIZE,DOWN_SIZE))
             crop_np = np.array(crop)
             crop_np = cv2.cvtColor(crop_np, cv2.COLOR_RGBA2RGB)
-            if not os.path.exists('data/downsample/'):
-                os.makedirs('data/downsample/')
-            cv2.imwrite('data/downsample/{}.png'.format(name), crop_np)
+            os.makedirs(os.path.join('data/globals/'), exist_ok=True)
+            cv2.imwrite('data/globals/{}.png'.format(name), crop_np)
             print('{} finished'.format(name))
+            time.s
     print("--- %s seconds ---" % (time.time() - start_time))
 
 def preprocess_patch(path_ori, width_resize=2560, height_resize=1920):
     start_time = time.time()
     all_files = os.listdir(path_ori)
-    if not os.path.exists('data/patches/'):
-        os.mkdir('data/patches/')
+    os.makedirs(os.path.join('data/locals/'), exist_ok=True)
 
     for file_ in all_files:
         ori = openslide.OpenSlide(os.path.join(path_ori, file_))
@@ -113,10 +110,9 @@ def preprocess_patch(path_ori, width_resize=2560, height_resize=1920):
         y_resize_mins = []
         y_resize_maxs = []
     
-        if os.path.exists('data/annotations/{}_annt.jpg'.format(name)):
-            if not os.path.exists('data/patches/' + name):
-                os.mkdir('data/patches/' + name)
-            annt_resized = cv2.imread('data/annotations/{}_annt.jpg'.format(name), 0)
+        if os.path.exists('data/masks/{}_annt.jpg'.format(name)):
+            os.makedirs(os.path.join('data/locals/' + name), exist_ok=True)
+            annt_resized = cv2.imread('data/masks/{}_annt.jpg'.format(name), 0)
             height_annt, width_annt = annt_resized.shape
             ret, score = cv2.threshold(annt_resized, 128, 1, 0)
             nLabels, labels, stats, centroids = cv2.connectedComponentsWithStats(score.astype(np.uint8), connectivity=4)
@@ -160,8 +156,7 @@ def preprocess_patch(path_ori, width_resize=2560, height_resize=1920):
             continue
 
         roi_h = x_max - x_min
-        roi_w = y_max - y_min
-        
+        roi_w = y_max - y_min      
         wp = int((roi_w - PATCH_SIZE) / STRIDE + 1)
         hp = int((roi_h - PATCH_SIZE) / STRIDE + 1)
         total = wp * hp
@@ -176,17 +171,28 @@ def preprocess_patch(path_ori, width_resize=2560, height_resize=1920):
                     continue
                 crop = ori.read_region((y,x), 0, (PATCH_SIZE,PATCH_SIZE))
                 crop = crop.convert('RGB')
-
                 if sum(ImageStat.Stat(crop).stddev)/3 < 18 or sum(ImageStat.Stat(crop).median)/3 > 200:
                     continue
-                if not os.path.exists('data/patches/{}'.format(name)):
-                    os.mkdir('data/patches/{}'.format(name))
-                crop.save('data/patches/{}/{}_{}_{}_{}_{}.png'.format(name,name,str(h),str(w),str(roi_h),str(roi_w)))
+                os.makedirs(os.path.join('data/locals/{}'.format(name)), exist_ok=True)
+                crop.save('data/locals/{}/{}_{}_{}_{}_{}.png'.format(name,name,str(h),str(w),str(roi_h),str(roi_w)))
                 if cnt % 1000 == 0:
                     print('{}/{}'.format(str(cnt), str(total)))
         print('{} finished'.format(name))
+        time.s
     print("--- %s seconds ---" % (time.time() - start_time_total))
+
 if __name__ == "__main__":
-    #preprocess_mask(path_ori)
-    #preprocess_global(path_ori)
-    preprocess_patch(path_ori)
+    import argparse
+    parser = argparse.ArgumentParser(description='Preprocessing')
+    parser.add_argument('--path_ori', type=str, default='/scratch2/zheng/ajpa2021-master/data/OSUWMC/', help='path to dataset where images store')
+    parser.add_argument('--path_mask', type=str, default='/scratch2/zheng/ajpa2021-master/data/OSUWMC_MASK/', help='path to dataset where masks (if possible) store')
+    parser.add_argument('--m', action='store_true', default=False, help='preprocess masks if possible')
+    parser.add_argument('--g', action='store_true', default=False, help='preprocess data at global level')
+    parser.add_argument('--p', action='store_true', default=False, help='preprocess data at patch level')
+    args = parser.parse_args()
+    if args.m:
+        preprocess_mask(args.path_ori, args.path_mask)
+    elif args.g:
+        preprocess_global(args.path_ori)
+    elif args.p:
+        preprocess_patch(args.path_ori)
